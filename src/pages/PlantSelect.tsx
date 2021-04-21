49705e69b28f7a6b83cde440f8207a954a50212c
JuniorN1 <junior_e.a.o@hotmail.com>
2021-04-21 18:13:08 -0300

    finish header component e change styles of plantselect.tsx

--- src/pages/PlantSelect.tsx
+++ src/pages/PlantSelect.tsx
@@ -4,17 +4,20 @@ import {
 } from 'react-native';
 import { Header } from '../components/header';
 import colors from '../styles/colors';
+import fonts from '../styles/fonts';
 export function PlantSelect(){
     return(
 
         <View style={styles.container}>
-            <Header/>
-            <Text>
-                Em qual ambiente
-            </Text>
-            <Text>
-                você quer colocar sua planta?
-            </Text>
+            <View style={styles.header}>
+                <Header/>
+                <Text style={styles.title}>
+                    Em qual ambiente
+                </Text>
+                <Text style={styles.subtitle}>
+                    você quer colocar sua planta?
+                </Text>
+            </View>
         </View>
 
     )
@@ -24,5 +27,23 @@ const styles = StyleSheet.create({
     container:{
         flex:1,
         backgroundColor:colors.background
+    },
+    header:{
+        paddingHorizontal:30
+    },
+    title:{
+        fontSize:17,
+        color:colors.heading,
+        fontFamily:fonts.heading,
+        lineHeight:20,
+        marginTop:15
+    },
+    subtitle:{
+        fontSize:17,
+        color:colors.heading,
+        fontFamily:fonts.text,
+        lineHeight:20,
+
+
     }
 })
