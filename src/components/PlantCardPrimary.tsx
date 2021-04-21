f5da2a28b66cf68c4881bd2c84b18f45924fe45e
JuniorN1 <junior_e.a.o@hotmail.com>
2021-04-21 19:32:22 -0300

    create component PlantCardPrimary

---
+++ src/components/PlantCardPrimary.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { 
+    StyleSheet,
+    Text,
+    Image,
+    View
+ } from 'react-native';
+import colors from '../styles/colors';
+import { 
+    RectButton,
+    RectButtonProps
+ } from 'react-native-gesture-handler';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+import fonts from '../styles/fonts';
+import { PlantSelect } from '../pages/PlantSelect';
+
+
+
+interface PlantsProps extends RectButtonProps{
+    data:{
+        name:string;
+        photo:string;
+    };
+
+}
+
+export const PlantCardPrimary = ({data,...rest}:PlantsProps) =>{
+    return(
+        <RectButton 
+            style={styles.container}
+            {...rest}
+        >
+            <Text style={styles.text}>
+                {data.name}
+            </Text>
+        </RectButton>
+    )
+}
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        maxWidth:'45%',
+        backgroundColor:colors.shape,
+        borderRadius:20,
+        paddingVertical:10,
+        alignItems:'center',
+        margin:10
+        
+    },
+    text:{
+        color:colors.green_dark,
+        fontFamily:fonts.heading,
+        marginVertical:16
+    }
+
+})
