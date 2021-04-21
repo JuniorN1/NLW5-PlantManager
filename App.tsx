d5ca0ce8136c2b951aa3edfb00f4e031779b24b5
JuniorN1 <junior_e.a.o@hotmail.com>
2021-04-21 14:05:18 -0300

    import Confirmation in app.tsx

--- App.tsx
+++ App.tsx
@@ -5,6 +5,7 @@ import {
 } from '@expo-google-fonts/jost';
 import AppLoading from 'expo-app-loading';
 import React from 'react';
+import { Confirmation } from './src/pages/Confirmation';
 import { UserIdentification } from './src/pages/UserIdentification';
 import { Welcome } from './src/pages/welcome';
 
@@ -19,7 +20,8 @@ export default function App() {
     return <AppLoading />;
   }
   return (
-    <UserIdentification/>
+    <Confirmation/>
+    // <UserIdentification/>
     // <Welcome/>
   );
 }
