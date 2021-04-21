833d0debaa2aab15f820830c62fdc66ba4793fed
JuniorN1 <junior_e.a.o@hotmail.com>
2021-04-21 11:17:28 -0300

    add loading await fonts not loadeads

--- App.tsx
+++ App.tsx
@@ -1,8 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
+
+import {
+  Jost_400Regular,
+  Jost_600SemiBold, useFonts
+} from '@expo-google-fonts/jost';
+import AppLoading from 'expo-app-loading';
 import React from 'react';
 import { Welcome } from './src/pages/welcome';
 
+
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    Jost_400Regular,
+    Jost_600SemiBold,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
   return (
     <Welcome/>
   );
